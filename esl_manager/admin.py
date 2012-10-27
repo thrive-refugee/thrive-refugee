26609348e8f80ad1618afe742a97b95373d50e18
Roger Roelofs <roger.roelofs@gmail.com>
2012-10-26 21:24:47 -0400

    add assesments and human readable field names

--- esl_manager/admin.py
+++ esl_manager/admin.py
@@ -1,9 +1,13 @@
-from esl_manager.models import ESLStudent,  Attended
+from esl_manager.models import ESLStudent,  Attended,  Assesment
 from django.contrib import admin
 
 class AttendedInline(admin.TabularInline):
     model = Attended
-    extra = 3
+    extra = 1
+
+class AssesmentInline(admin.TabularInline):
+    model = Assesment
+    extra = 1
 
 class ESLStudentAdmin(admin.ModelAdmin):
     fieldsets = [
@@ -11,7 +15,7 @@ class ESLStudentAdmin(admin.ModelAdmin):
         ('Address', {'fields': ['StreetAddress', 'City', 'State', 'Zip']}),
         ('Phones',               {'fields': ['Phones']}),
     ]
-    inlines = [AttendedInline]
+    inlines = [AttendedInline, AssesmentInline]
     list_display = ('FirstName', 'LastName', 'StreetAddress', 'Phones')
     list_filter = ['FirstName', 'LastName']
     search_fields = ['FirstName', 'LastName']
--- esl_manager/models.py
+++ esl_manager/models.py
@@ -4,9 +4,9 @@ import datetime
 # Create your models here.
 
 class ESLStudent(models.Model):
-    FirstName = models.CharField(max_length=50)
-    LastName = models.CharField(max_length=80)
-    StreetAddress = models.CharField(max_length=50)
+    FirstName = models.CharField("First Name",  max_length=50)
+    LastName = models.CharField("Last Name", max_length=80)
+    StreetAddress = models.CharField("Address", max_length=50)
     City = models.CharField(max_length=20)
     State = models.CharField(max_length=2)
     Zip = models.CharField(max_length=10)
@@ -18,8 +18,16 @@ class ESLStudent(models.Model):
 
 class Attended(models.Model):
     esl_tudent = models.ForeignKey(ESLStudent)
-    attend_date = models.DateTimeField('attend_date')
+    attend_date = models.DateField('Attended')
 
     def __unicode__(self):
         return self.attend_date
 
+
+class Assesment(models.Model):
+    esl_tudent = models.ForeignKey(ESLStudent)
+    taken_date = models.DateField('Taken')
+    Score = models.IntegerField()
+
+    def __unicode__(self):
+        return self.taken_date
