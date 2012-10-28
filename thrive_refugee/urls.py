19f11c189bf04cc910fcc954bbd1dbacf9291a51
Roger Roelofs <roger.roelofs@gmail.com>
2012-10-27 23:14:45 -0400

    git goofed?

--- thrive_refugee/urls.py
+++ thrive_refugee/urls.py
@@ -13,5 +13,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', include(admin.site.urls)),
-    url(r'^refugee_manager/', include('refugee_manager.urls')), 
+    url(r'^refugee_manager/', include('refugee_manager.urls')),  // get git to update
 )
