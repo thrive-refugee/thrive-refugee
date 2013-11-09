3841f1af80ac34aa7c0bddafd1c8afd79540147f
Jamie Bliss <astronouth7303@gmail.com>
2013-11-09 13:50:46 -0500

    Try harder to find the user

--- swingtime/templatetags/ical.py
+++ swingtime/templatetags/ical.py
@@ -12,4 +12,10 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def ical(context, protocol='webcal', **kw):
-	return ICal_Calendar.genurl(context['user'], protocol=protocol, **kw)
+	if 'user' in context:
+		user = context['user']
+	elif 'request' in context:
+		user = context['request'].user
+	else:
+		raise IndexError("Misconfigured context processors, can't find current user")
+	return ICal_Calendar.genurl(user, protocol=protocol, **kw)
