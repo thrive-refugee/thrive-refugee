f6a621ea3c69f9b667c5118838f97d811364d65c
Ben Rousch <brousch@gmail.com>
2013-11-10 11:46:10 -0500

    Better show/hide of frequency

--- refugee_manager/static/js/recurring_form.js
+++ refugee_manager/static/js/recurring_form.js
@@ -1,11 +1,21 @@
 $(document).ready(function() {
 
     hide_frequency();
-    $("[name='repeats']").click(show_frequency);
-    $("#id_until").click(function() {
-        $("input[name=repeats][value=until]").click();
-    });
-    $("#id_count").change(show_frequency);
+    $("#id_repeats_0").click(function() {
+		if ($("#id_count").val() > 1) {
+			show_frequency();
+		} else {
+			hide_frequency();
+		}
+	});
+    $("#id_count").change(function() {
+		if ($("#id_count").val() > 1) {
+			show_frequency();
+		} else {
+			hide_frequency();
+		}
+	});
+	$("#id_repeats_1").click(show_frequency);
     $("#id_day").datepicker();
     $("#id_until").datepicker();
 });
