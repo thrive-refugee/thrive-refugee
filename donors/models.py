0b7c4d56a853563623d767f21ce3ac5f4aae68b8
Jamie Bliss <astronouth7303@gmail.com>
2014-11-07 21:48:43 -0500

    Basic donor model

--- donors/models.py
+++ donors/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+
+class Donor(models.Model):
+    name = models.CharField()
+    business = models.CharField(blank=True)
+    email = models.EmailField(blank=True)
+    phone = models.CharField(max_length=10+3)  # django.contrib.localflavor.us.forms.USPhoneNumberField
+    address = models.TextField(blank=True)
+    last_amount = models.DecimalField(decimal_places=2)
+    last_donation = models.DateField(default=datetime.date.today)
+    notes = models.TextField(blank=True)
