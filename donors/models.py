9c4cdb85a7b35bc298bf47f09d7bee41be68bcb3
Jamie Bliss <astronouth7303@gmail.com>
2014-11-07 23:51:54 -0500

    Make phone number optional

--- donors/models.py
+++ donors/models.py
@@ -8,7 +8,7 @@ class Donor(models.Model):
     name = models.CharField(max_length=64)
     business = models.CharField(max_length=64, blank=True)
     email = models.EmailField(blank=True)
-    phone = localflavor.us.models.PhoneNumberField()
+    phone = localflavor.us.models.PhoneNumberField(null=True)
     address = models.TextField(blank=True)
     last_amount = models.DecimalField(max_digits=9+2, decimal_places=2)
     last_donation = models.DateField(default=datetime.date.today)
