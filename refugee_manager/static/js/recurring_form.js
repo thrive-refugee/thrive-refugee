9ab0c4cc0ec33672e269522e749cf3509b358912
Jamie Bliss <astronouth7303@gmail.com>
2014-11-08 00:32:08 -0500

    Check new box. FIXME: Uncheck old one

--- refugee_manager/static/js/recurring_form.js
+++ refugee_manager/static/js/recurring_form.js
@@ -2,33 +2,41 @@ $(document).ready(function() {
 
     hide_frequency();
     $("#id_repeats_0").click(function() {
-		if ($("#id_count").val() > 1) {
-			show_frequency();
-		} else {
-			hide_frequency();
-		}
-	});
+        if ($("#id_count").val() > 1) {
+            show_frequency();
+        } else {
+            hide_frequency();
+        }
+    });
     $("#id_count").keyup(function() {
-		if ($("#id_count").val() > 1) {
-			show_frequency();
-		} else {
-			hide_frequency();
-		}
-	});
-	$("#id_repeats_1").click(show_frequency);
+        if ($("#id_count").val() > 1) {
+            show_frequency();
+        } else {
+            hide_frequency();
+        }
+    });
+    $("#id_repeats_1").click(show_frequency);
     $("#id_day").datepicker();
+
+    $('#id_day').change(function() {
+        var date = new Date($('#id_day').val());
+        var dow = date.getDay();
+        $('#id_week_days_'+dow).attr('checked', 'checked');
+        // FIXME: Deselect old day.
+    });
+
     $("#id_until").datepicker();
 
     $('#id_refugee_case').change(function() {
-	if ($(this).val()) {
-	    $('#id_employment_case').val('');
-	}
+        if ($(this).val()) {
+            $('#id_employment_case').val('');
+        }
     });
 
     $('#id_employment_case').change(function() {
-	if ($(this).val()) {
-	    $('#id_refugee_case').val('');
-	}
+        if ($(this).val()) {
+            $('#id_refugee_case').val('');
+        }
     });
 });
 
