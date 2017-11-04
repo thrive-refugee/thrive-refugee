2e38989190deeec117274467da91da61b53226ae
Jamie Bliss <astronouth7303@gmail.com>
2017-11-04 19:32:37 -0400

    Add static route if DEBUG (ie heroku)

--- thrive_refugee/urls.py
+++ thrive_refugee/urls.py
@@ -13,3 +13,8 @@ urlpatterns = patterns(
     url(r'^calendar/', include('swingtime.urls')),
     url(r'^uploads/(?P<filename>.*)$', views.serve_file),
 )
+
+from django.conf import settings
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
